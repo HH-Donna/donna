2471ff54162cf095ded726ac699d6a6b4524f8c1
Allen <allen.brd.75@gmail.com>
2025-10-04 19:23:28 -0400

    remove useless

--- api/add_user_account_number.sql
+++
@@ -1,7 +0,0 @@
--- Add user_account_number field to companies table
--- Run this in your Supabase SQL Editor
-
-ALTER TABLE public.companies 
-  ADD COLUMN IF NOT EXISTS user_account_number TEXT DEFAULT '';
-
-COMMENT ON COLUMN public.companies.user_account_number IS 'User''s account/client/customer number with this biller (e.g., "Account: A-12345", "Customer ID: 98765")';
